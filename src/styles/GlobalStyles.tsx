96762bced8788b610898b3cfd79e03ae2079ccee
Oleksandr <didyshen.oleksandr@gmail.com>
2023-06-24 19:05:18 +0300

    Add Manrope font

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,5 +1,11 @@
 import * as styled from 'styled-components';
 
-const GlobalStyle = styled.createGlobalStyle``;
+const GlobalStyle = styled.createGlobalStyle`
+  html {
+    font-family: Manrope, system-ui, -apple-system, BlinkMacSystemFont,
+      'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+      'Helvetica Neue', sans-serif;
+  }
+`;
 
 export default GlobalStyle;
