119446edc6478fb5aa7855ea6befc66320b77831
Oleksandr Didyshen <102871422+alex-dishen@users.noreply.github.com>
2023-06-25 11:23:28 +0300

    Setup backend for the project (#9)

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express, { json } from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = 8000
+
+app.use(cors())
+app.use(json())
+
+app.listen(port, () =>
+  console.log(
+    `⚡️[server]: Game Harbor listening on port http://localhost:${port}!`
+  )
+)
